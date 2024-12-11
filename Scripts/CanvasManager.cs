375ca8a7c3e4051828303cc503dabc9ff4f8cc89
krschan <krischxn@gmail.com>
2024-12-11 16:32:04 +0100

    FEAT: player can pause the game.

--- Scripts/CanvasManager.cs
+++ Scripts/CanvasManager.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject buttonPause;
+    [SerializeField] private GameObject menuPause;
+
+    private void Start()
     {
-        
+        Time.timeScale = 1f;
+        buttonPause.SetActive(true);
+        menuPause.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void clickPause()
     {
-        
+        Time.timeScale = 0f;
+        buttonPause.SetActive(false);
+        menuPause.SetActive(true);
+    }
+
+    public void clickResume()
+    {
+        Time.timeScale = 1f;
+        buttonPause.SetActive(true);
+        menuPause.SetActive(false);
+    }
+
+    public void clickRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
